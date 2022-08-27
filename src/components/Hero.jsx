faac87b09fa572898c2aeae83fc2828084aafd5a
amine <mohamedamineguesmi@insat.u-carthage.tn>
2022-08-27 10:06:53 +0100

    an other fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import image from '../../public/assets/Group 77.png'
 const Hero = () => {
   return (
         <section className="hero">
-      <img src="/assets/Group 77.png" alt="" />
+      <img src={image} alt="" />
         <h1>Online Experiences</h1>
         <p>Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
